f3a9b1f910af0dd0b463fc7a4933c2f33c80090a
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-02-10 17:42:08 +0000

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -20,7 +20,7 @@
 
     function encryptText(text) {
         const key1 = decodeObfuscatedKey([69, 162, 69, 219, 5, 234, 249, 17, 198, 96, 240, 195, 12, 21, 54, 11, 20, 41, 65, 104]);
-        const key2 = decodeObfuscatedKey([69, 162, 69, 219, 5, 234, 249, 17, 198, 96, 240, 195, 12, 21, 54, 11, 20, 41, 65, 107]);
+        const key2 = decodeObfuscatedKey([2, 255, 14, 14, 252, 147, 2, 252, 246, 255, 2, 255, 249, 207, 68, 252, 5, 255, 171, 2, 240, 147, 2, 204, 243, 144, 2, 252, 255, 144, 14, 2, 252, 207, 104, 14, 246, 207, 2, 204, 243, 144, 246, 210, 246, 207, 68, 255]);
         let encrypted = xorEncryptDecrypt(text, key1);
         encrypted = xorEncryptDecrypt(encrypted, key2);
         return customEncode(encrypted);
