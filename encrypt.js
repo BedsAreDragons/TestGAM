3f84e9918ce3672590ba3ebf8f2775f00dfb9de9
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-02-10 19:17:54 +0000

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -2,55 +2,48 @@
     function xorEncryptDecrypt(input, key) {
         let output = '';
         for (let i = 0; i < input.length; i++) {
-            output += String.fromCharCode(
-                (input.charCodeAt(i) ^ key.charCodeAt(i % key.length)) + ((i * 3) % 7)
-            );
+            output += String.fromCharCode(input.charCodeAt(i) ^ key.charCodeAt(i % key.length));
         }
         return output;
     }
 
     function complexEncode(input) {
-        return input.split('').map((c, i) => {
-            let code = c.charCodeAt(0);
-            return String.fromCharCode(((code * 7 + i * 2) % 256) ^ (173 + (i % 5)));
-        }).reverse().join('');
+        return btoa(input.split('').map((c, i) => 
+            String.fromCharCode(c.charCodeAt(0) + (i % 7))
+        ).join(''));
+    }
+
+    function complexDecode(input) {
+        return atob(input).split('').map((c, i) => 
+            String.fromCharCode(c.charCodeAt(0) - (i % 7))
+        ).join('');
     }
 
     function obfuscateKey(arr) {
-        return arr.map(n => String.fromCharCode((n ^ 42) + 5)).reverse().join('');
-    }
-
-    function dynamicKey(seed) {
-        let key = '';
-        for (let i = 0; i < seed.length; i++) {
-            key += String.fromCharCode(((seed.charCodeAt(i) * 13) % 256) ^ 91);
-        }
-        return key;
-    }
-
-    function shuffleString(str) {
-        let arr = str.split('');
-        for (let i = arr.length - 1; i > 0; i--) {
-            let j = (Math.floor(Math.random() * (i + 1)) + i) % arr.length;
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-        return arr.join('');
+        return arr.map(n => String.fromCharCode(n ^ 42)).join('');
     }
 
     function encryptText(text) {
-        const seedKey = dynamicKey(text);
-        const key1 = obfuscateKey([120, 97, 110, 115, 111, 109, 101, 116, 104, 105, 110, 103]);
-        const key2 = obfuscateKey([115, 101, 99, 117, 114, 105, 116, 121, 107, 101, 121]);
+        const key1 = obfuscateKey([99, 111, 109, 112, 108, 101, 120]);
+        const key2 = obfuscateKey([115, 101, 99, 117, 114, 105, 116, 121]);
         let encrypted = xorEncryptDecrypt(text, key1);
         encrypted = xorEncryptDecrypt(encrypted, key2);
-        encrypted = xorEncryptDecrypt(encrypted, seedKey);
-        encrypted = complexEncode(encrypted);
-        return shuffleString(encrypted);
+        return complexEncode(encrypted);
+    }
+
+    function decryptText(encryptedText) {
+        const key1 = obfuscateKey([99, 111, 109, 112, 108, 101, 120]);
+        const key2 = obfuscateKey([115, 101, 99, 117, 114, 105, 116, 121]);
+        let decrypted = complexDecode(encryptedText);
+        decrypted = xorEncryptDecrypt(decrypted, key2);
+        decrypted = xorEncryptDecrypt(decrypted, key1);
+        return decrypted;
     }
 
     if (typeof module !== 'undefined' && module.exports) {
-        module.exports = encryptText;
+        module.exports = { encryptText, decryptText };
     } else {
         window.encryptText = encryptText;
+        window.decryptText = decryptText;
     }
 })();
