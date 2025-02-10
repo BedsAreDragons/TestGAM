7ddd980dc001d509b9aa3a8a5cc4318e5dfd760b
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-02-10 17:52:11 +0000

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -14,13 +14,13 @@
         }).join('');
     }
 
-    function decodeObfuscatedKey(arr) {
+    function obfuscateKey(arr) {
         return arr.map(n => String.fromCharCode(n)).join('');
     }
 
     function encryptText(text) {
-        const key1 = decodeObfuscatedKey([69, 162, 69, 219, 5, 234, 249, 17, 198, 96, 240, 195, 12, 21, 54, 11, 20, 41, 65, 104]);
-        const key2 = decodeObfuscatedKey([2, 255, 14, 14, 252, 147, 2, 252, 246, 255, 2, 255, 249, 207, 68, 252, 5, 255, 171, 2, 240, 147, 2, 204, 243, 144, 2, 252, 255, 144, 14, 2, 252, 207, 104, 14, 246, 207, 2, 204, 243, 144, 246, 210, 246, 207, 68, 255]);
+        const key1 = obfuscateKey([86, 109, 48, 120, 77, 70, 89, 120, 86, 88, 104, 97, 82, 109, 82, 86, 89, 84, 70, 119, 85, 49, 82, 87, 86, 110, 100, 83, 77, 86, 112, 121, 86, 109, 115, 53, 86, 108, 74, 114, 99, 69, 100, 87, 98, 71, 104, 110, 85, 70, 69, 57, 80, 81, 61, 61]);
+        const key2 = obfuscateKey([86, 109, 112, 67, 86, 49, 82, 87, 85, 107, 90, 86, 98, 69, 74, 86, 89, 49, 65, 122, 83, 70, 90, 69, 82, 75, 116, 85, 98, 69, 53, 87, 86, 70, 81, 119, 80, 81, 61, 61]);
         let encrypted = xorEncryptDecrypt(text, key1);
         encrypted = xorEncryptDecrypt(encrypted, key2);
         return customEncode(encrypted);
